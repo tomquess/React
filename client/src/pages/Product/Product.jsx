b66f8ec8d6d9a0dfceb50c7f72c915481030deda
Tomasz Czarzbon <tomques11@gmail.com>
2023-01-26 19:48:42 +0100

    cart and list

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import { useState } from 'react'
 import "./Product.scss"
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart"
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder"
+import BalanceIcon from "@mui/icons-material/Balance"
 
 const Product = () => {
+    const [selectedImg,setSelectedImg] = useState(0);
+    const [quantity,setQuantity] = useState(1)
+
+    const images = [
+        "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    ]
     return (
-        <div className='product'>Product</div>
+        <div className='product'>
+            <div className="left">
+                <div className="images">
+                <img src={images[0]} alt="" onClick={e=>setSelectedImg(0)}/>
+                <img src={images[1]} alt="" onClick={e=>setSelectedImg(1)}/>
+                </div>
+                <div className="mainImg">
+                    <img src={images[selectedImg]} alt="" />
+                </div>
+            </div>
+
+            <div className="right">
+                <h1>Title</h1>
+                <span>$199</span>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint inventore libero nam odit accusantium numquam 
+                    voluptatem doloribus dicta fugit eaque quidem ducimus labore voluptate doloremque quam facilis, quasi ex natus!
+                </p>
+                <div className="quantity">
+                    <button onClick={()=>setQuantity(prev=>prev === 1 ? 1 : prev-1)}>-</button>
+                    {quantity}
+                    <button onClick={()=>setQuantity(prev=>prev+1)}>+</button>
+                </div>
+                <button className='add'>
+                    <AddShoppingCartIcon /> ADD TO CART
+                </button>
+                <div className="links">
+                    <div className="item">
+                        <FavoriteBorderIcon /> ADD TO WISHLIST
+                    </div>
+                    <div className="item">
+                        <BalanceIcon /> ADD TO COMPARE
+                    </div>
+                </div>
+                <div className="info">
+                    <span>Vendor: Polo</span>
+                    <span>Product Type: T-Shirt</span>
+                    <span>Tag: T-Shirt, Women, Top</span>
+                </div>
+                <hr />
+                <div className="details">
+                    <span>DESCRIPTION</span>
+                    <hr />
+                    <span>ADDITIONAL INFORMATION</span>
+                    <hr />
+                    <span>FAQ</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
